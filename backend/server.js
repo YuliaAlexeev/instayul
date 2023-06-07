8f38493e57b2dc9c7b22b7f8d12149a6ff15accf
Yulia <july90a@gmail.com>
2023-06-07 15:16:07 +0300

    port change, and package.json scripts

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,8 @@ if (process.env.NODE_ENV === "production") {
     origin: [
       "http://127.0.0.1:8080",
       "http://localhost:8080",
-      "http://127.0.0.1:3050",
-      "http://localhost:3050",
+      "http://127.0.0.1:3000",
+      "http://localhost:3000",
     ],
     credentials: true,
   };
